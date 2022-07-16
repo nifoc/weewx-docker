256d076aff61ebe9d772a42838d7040afc90b54a
Daniel Kempkens <daniel+git@kempkens.io>
2022-07-16 13:31:14 +0200

    Flake for development

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "weewx environment";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/22.05";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            python310
+          ];
+        };
+      });
+}
